75e7f030327ef2d67805685f681109f3c572210b
Zack Clobes <zack@custom-ds.com>
2026-07-12 13:15:08 -0500

    26MHz TCXO and version 2.0.0 of PCB

--- firmware/ptSolarHF/ptSolarHF.ino
+++ firmware/ptSolarHF/ptSolarHF.ino
@@ -118,7 +118,7 @@ void setup() {
     wdt_reset();
 
     Serial.println(F("Init Si5351"));
-    si5351.init(SI5351_CRYSTAL_LOAD_10PF, 27000000, 0);
+    si5351.init(SI5351_CRYSTAL_LOAD_0PF, 26000000, 0);
     si5351.set_correction(Config.getCorrection(), SI5351_PLL_INPUT_XO);
     si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_8MA);
     si5351.output_enable(SI5351_CLK0, 0);       //Turn off any outputs for now
